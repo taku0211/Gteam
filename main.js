8a3a80478137be383289c64a98281838c7e3e506
y21507 <y21507@iwate-it.ac.jp>
2021-12-22 15:09:43 +0900

    a
    a

--- main.js
+++ main.js
@@ -53,14 +53,16 @@ upgrade_button.addEventListener("click", function(){
     }
 });
 
-let weapon=document.getElementById("weapon");
 let clickpoint = document.getElementById('clickpoint');
 weapon.onclick = function() {
     quantity[phase]+=click;
     point_dom.innerHTML = point;
+    $('#clickpoint').fadeOut();
+    clickpoint.innerHTML = clickpoint;
     setTimeout(function(){
-    clickpoint.style.display = "none";
-    }, 1000);
+        clickpoint.style.display = "none";
+        $('#clickpoint').fadeOut();
+        }, 1000);
 }
 
 
