30f8d9a9560fa367d2de7db0f2d1f5bd4135c7cf
takuma <96404862+taku0211@users.noreply.github.com>
2021-12-22 15:09:22 +0900

    a
    a

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
-let phase=0;
-let point=1;
+let phase=0;    //フェーズ。何を今作っているか。例:0=ナイフ、1=ロングソード
+let point=1;    
 let point_dom = document.getElementById("click");
 let click=1;
-let gold=5000;
+let gold=5000;  //ここの値が初期の所持金になる
 
-let weapon=document.getElementById("weapon");
-const weapon_name = new Array("ナイフ", "ロングソード", "デュアルソード","刀","ファイアソード");
-const weapon_png = new Array("ナイフ.png", "ロングソード.png", "デュアルソード.png","刀.png","ファイアソード.png")
-let now_weapon=document.getElementById("now_weapon");
-let now_weapon_png=document.getElementById("now_weapon_png");
-let next_weapon=document.getElementById("next_weapon");
-let next_weapon_png=document.getElementById("next_weapon_png");
+/* アップグレードで使ったやつ -------------------------- */
 
-let upgrade_alert=document.getElementById("upgrade_alert");
-const upgrade_price = new Array(5000, 10000, 20000,30000,50000);
-let now_gold=document.getElementById("now_g");
-let upgrade_button=document.getElementById("upgrade_button");
+let weapon=document.getElementById("weapon"); //左画面の武器の画像
+const weapon_name = new Array("ナイフ", "ロングソード", "デュアルソード","刀","ファイアソード");    //武器名を入れた配列
+const weapon_png = new Array("ナイフ.png", "ロングソード.png", "デュアルソード.png","刀.png","ファイアソード.png")  //武器の画像名を入れた配列
+let now_weapon=document.getElementById("now_weapon");           //現在作っている武器の名前
+let now_weapon_png=document.getElementById("now_weapon_png");   //右画面の、現在作っている武器の画像
+let next_weapon=document.getElementById("next_weapon");         //次に作る武器の名前
+let next_weapon_png=document.getElementById("next_weapon_png"); //右画面の、次に作る武器の画像
 
+let upgrade_alert=document.getElementById("upgrade_alert");     //アップグレード時のアラートメッセージ
+const upgrade_price = new Array(5000, 10000, 20000,30000,50000);//アップグレードに必要な価格を入れた配列
+let now_gold=document.getElementById("now_g");                  //右画面の、現在の所持金を出す部分
+let upgrade_button=document.getElementById("upgrade_button");   //工房のアップグレードボタン 
+
+/*　アップグレード時のアラートメッセージを空白に*/
+let upgrade_alert_reset=function(){ 
+    upgrade_alert.innerHTML="";
+}
+
+/*　現在の所持金を表示 */
 let gold_count=function(){
     now_gold.innerHTML="現在の所持金:"+gold+"G";
 }
 
-let upgrade_alert_reset=function(){
-    upgrade_alert.innerHTML="";
-}
+/* ----------------------------------------- */
 
 window.addEventListener("DOMContentLoaded", function () {
     quantity=localStorage;  //武器の個数の配列
@@ -31,29 +37,28 @@ window.addEventListener("DOMContentLoaded", function () {
         quantity[i]=0;
     }
     point_dom.innerHTML = point;
-    setInterval(gold_count,100);
+    setInterval(gold_count,100);    //0.1秒ごとに、現在の所持金の表示を更新
 });
 
 upgrade_button.addEventListener("click", function(){
     if(upgrade_price[phase]<=gold){
-        gold-=upgrade_price[phase];
-        upgrade_alert.innerHTML="アップグレードに成功しました!("+upgrade_price[phase]+"G消費)";
-        phase+=1;
-        click=1;
-        point=1;
-        now_weapon.innerHTML=weapon_name[phase];
-        next_weapon.innerHTML=weapon_name[phase+1];
-        now_weapon_png.src="images/"+weapon_png[phase];
-        next_weapon_png.src="images/"+weapon_png[phase+1];
-        weapon.src="images/"+weapon_png[phase];
-        setTimeout(upgrade_alert_reset,1500);
+        gold-=upgrade_price[phase]; //アップグレード分のお金を引く
+        upgrade_alert.innerHTML="アップグレードに成功しました!("+upgrade_price[phase]+"G消費)"; //アラートメッセージ表示
+        phase+=1;   //フェーズを進める(次の武器を作るので)
+        click=1;    //一度に作れる個数を1にリセット
+        point=1;    //一度に作れる個数を1にリセット
+        now_weapon.innerHTML=weapon_name[phase];            //アップグレード画面のテキストを次の武器に更新
+        next_weapon.innerHTML=weapon_name[phase+1];         //アップグレード画面のテキストを次の武器に更新
+        now_weapon_png.src="images/"+weapon_png[phase];     //アップグレード画面の画像を次の武器に更新
+        next_weapon_png.src="images/"+weapon_png[phase+1];  //アップグレード画面の画像を次の武器に更新
+        weapon.src="images/"+weapon_png[phase];             //左画面の画像を次の武器に更新
+        setTimeout(upgrade_alert_reset,1500);               //1.5秒後、アラートメッセージ削除
     }else{
-        upgrade_alert.innerHTML="アップグレードに失敗しました。("+(upgrade_price[phase]-gold)+"G不足)";
-        setTimeout(upgrade_alert_reset,1500);
+        upgrade_alert.innerHTML="アップグレードに失敗しました。("+(upgrade_price[phase]-gold)+"G不足)"; //アラートメッセージ表示
+        setTimeout(upgrade_alert_reset,1500);   //1.5秒後、アラートメッセージ削除
     }
 });
 
-let weapon=document.getElementById("weapon");
 let clickpoint = document.getElementById('clickpoint');
 weapon.onclick = function() {
     quantity[phase]+=click;
