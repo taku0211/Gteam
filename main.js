b119b2b4d2542bfab118a904e6410197192b8eaa
takuma <96404862+taku0211@users.noreply.github.com>
2021-12-22 10:32:27 +0900

    aa
    a

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-var weapon=0;
-document.getElementsByClassName("weapon").onclick = function() {
+let phase=0;
+let click=1;
+window.addEventListener("DOMContentLoaded", function () {
+    quantity=localStorage;  //武器の個数の配列
+    for(let i=0;i<5;i++){   //武器の個数を初期化
+        quantity[i]=0;
+    }
+    let count=document.getElementsById("count");
+    let count_int=quantity[0];
+    count.appendChild(count_int);
+});
+
+let weapon=document.getElementsById("weapon")
+weapon.onclick = function(phase,click) {
+    quantity[phase]+=click;
 }
