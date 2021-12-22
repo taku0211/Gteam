2408bd109e4f7ab53dd1f89a51fc37bcd43cd55f
y21507 <y21507@iwate-it.ac.jp>
2021-12-22 13:35:01 +0900

    a
    a

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 let phase=0;
-let click=1;
+let point=1;
+let point = document.getElementById("click");
+
 window.addEventListener("DOMContentLoaded", function () {
     quantity=localStorage;  //武器の個数の配列
     for(let i=0;i<5;i++){   //武器の個数を初期化
         quantity[i]=0;
     }
-    let count=document.getElementsById("count");
-    let count_int=quantity[0];
-    count.appendChild(count_int);
+    point.innerHTML = point;
 });
 
-let weapon=document.getElementsById("weapon")
+let weapon=document.getElementById("weapon");
 weapon.onclick = function(phase,click) {
     quantity[phase]+=click;
+    click++;
+    point.innerHTML = point;
 }
